bad58514b4f5a130dc9e2fd93d17469d1b71b8ec
Samuel555100 <66750528+Samuel555100@users.noreply.github.com>
2023-09-11 19:25:01 -0600

    Add files via upload

---
+++ Sesión 10/Samuel_Bonilla_sesion10/libreria/SamuelBonillaSesion10.py
@@ -0,0 +1,8 @@
+"""Imagina que estás creando un sistema para administrar una biblioteca de libros. 
+Quieres organizar tus funciones en módulos para mantener el código ordenado y modular. 
+Utilizarás módulos incorporados como datetime para manejar fechas.
+
+Crea un archivo llamado biblioteca.py que contendrá tu programa principal y 
+un módulo personalizado llamado funciones_biblioteca.py que contendrá las funciones 
+relacionadas con la administración de la biblioteca."""
+
